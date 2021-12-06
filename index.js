7f50669ae3ee9d3f6d9c7c1aa2fd2bd55b75a381
rohan-chari <86305999+rohan-chari@users.noreply.github.com>
2021-12-06 00:06:50 -0500

    connected bot to server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-require('dotenv').config();
+const config = require('dotenv').config();
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(process.env.CLIENT_TOKEN);
+
