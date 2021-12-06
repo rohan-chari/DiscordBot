a27682e6b3461d86628734951fe20101cca3b67f
rohan-chari <86305999+rohan-chari@users.noreply.github.com>
2021-12-06 15:12:59 -0500

    Weather API working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('dotenv').config();
-
+const axios = require('axios');
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 
 const { Client, Intents } = require('discord.js');
@@ -9,8 +10,30 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(process.env.WEATHER_TOKEN);
 });
+client.on('messageCreate', async msg => {
+    if(msg.content.includes("!weather")){
+      let zipCode = msg.content.split(" ")[1];
+      if(zipCode === undefined || zipCode.length != 5 || parseInt(zipCode)===NaN){
+        msg.channel.send("Invalid ZipCode! Follow the format: !weather XXXXX").catch(console.error)
+        return;
+      }else{
+        fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&appid=' + process.env.WEATHER_TOKEN)
+        .then(response => response.json()).then(data => console.log(data));
+      }
+    }
+})
+
+
+
+
+
+
+  
+
 
 client.login(process.env.CLIENT_TOKEN);
 //testing
+
 
